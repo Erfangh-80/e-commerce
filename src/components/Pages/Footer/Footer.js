ccbad884ea58c70c09e381ff1879fa45f7104db0
Erfan gholami <Erfangh4871gholami@gmail.com>
2022-07-29 18:58:56 +0430

    added copy rigth component to footer

--- src/components/Pages/Footer/Footer.js
+++ src/components/Pages/Footer/Footer.js
@@ -3,6 +3,7 @@ import React from "react";
 import Contents from "./ContentFooter/Contents.js";
 import AboutUs from "./ContentFooter/AboutUs.js";
 import Follow from "./ContentFooter/Follow.js";
+import CopyRight from "./CopyRight.js";
 
 import Styles from "../../../assets/styles/Footer.module.css";
 
@@ -15,6 +16,7 @@ const Footer = () => {
         <Contents />
         <Follow />
       </div>
+      <CopyRight />
     </footer>
   );
 };
