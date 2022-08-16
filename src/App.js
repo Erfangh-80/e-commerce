228edf6de58e464e99265d9b2b7bbe8cf6d1bb2c
Erfan gholami <Erfangh4871gholami@gmail.com>
2022-08-16 16:48:52 +0430

    add comment to app components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
+// styles
 import Styles from "./App.css";
-
+// components
 import Navbar from "./components/template/Navbar/Navbar.js";
 import Home from "./components/Pages/Home/HomePage.js";
 import SignUp from "./components/Pages/Form/SignUp.js";
 import Login from "./components/Pages/Form/Login.js";
 import Products from "./components/Pages/Products/Products";
-import ProductsContextProvider from "./context/ProductsContextProvider";
+import DetailProduct from "./components/Pages/DetailProduct/DetailProduct.js";
+// context
+import ProductsContextProvider from "./context/ProductsContextProvider.js";
+import CartContextProvider from "./context/CartContextProvider.js";
 
 const App = () => {
   return (
     <div className={Styles.container}>
+      <Navbar />
       <ProductsContextProvider>
-        <Navbar />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/homepage" component={Home} />
-          <Route path="/products" component={Products} />
-          <Redirect to="/homepage" />
-        </Switch>
+        <CartContextProvider>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/homepage" component={Home} />
+            <Route path="/products" component={Products} />
+            <Route path="/detailproduct/:id" component={DetailProduct} />
+            <Route path="/detailproduct" component={DetailProduct} />
+            <Redirect to="/homepage" />
+          </Switch>
+        </CartContextProvider>
       </ProductsContextProvider>
     </div>
   );
