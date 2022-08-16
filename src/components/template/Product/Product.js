53e5e9dbd966ad91a3634ff4e2be981fb1328658
Erfan gholami <Erfangh4871gholami@gmail.com>
2022-08-16 13:18:58 +0430

    Dynamic button component

--- src/components/template/Product/Product.js
+++ src/components/template/Product/Product.js
@@ -1,22 +1,36 @@
-import React from "react";
-import { shorten } from "../../../helper/function";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
+// context
+import { CartContext, ACTIONS } from "../../../context/CartContextProvider.js";
+// helpers
+import { shorten, isInCart, quantityCount } from "../../../helper/function";
+// icons
+import trash from "../../../assets/icons/trash.svg"
+// styles
 import styles from "./Product.module.css";
 
 const Product = (props) => {
-  const { title, imageSrc, price } = props;
+  const { state, dispatch } = useContext(CartContext);
 
+  const { id, title, imageSrc, price } = props;
   return (
     <div className={styles.container}>
       <img className={styles.cardImage} src={imageSrc} alt=" product " />
       <h3>{shorten(title)}</h3>
       <p>{price} $</p>
       <div className={styles.linkContainer}>
-        <a href="/">Detail</a>
+        <Link to={`/detailproduct/${id}`}>Detail</Link>
         <div className={styles.buttonContainer}>
-          <button>Add to Cart</button>
+          {
+            isInCart(state, id) 
+            ? <button className={styles.smallButton} onClick={() => dispatch({type: ACTIONS.INCREASE, payload: props})}>+</button>
+            : <button onClick={() => dispatch({type: ACTIONS.ADD_ITEM, payload: props})}>Add to Cart</button>
+          }
+          {quantityCount(state, id) === 1 && <button className={styles.smallButton} onClick={() => dispatch({type: ACTIONS.REMOVE_ITEM, payload: props})}><img src={trash} alt="trash"/></button>}
+          {quantityCount(state, id) > 1 && <button className={styles.smallButton} onClick={() => dispatch({type: ACTIONS.DECREASE, payload: props})}>-</button>}
         </div>
-      </div>
+      </div>switch
     </div>
   );
 };
