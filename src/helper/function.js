3fc332cbc9cabcdf6249ddf32bd55897b8a578f5
Erfan gholami <Erfangh4871gholami@gmail.com>
2022-08-16 12:14:06 +0430

    add new function to project

--- src/helper/function.js
+++ src/helper/function.js
@@ -1,8 +1,20 @@
-
-
 const shorten = (title) => {
-    const newTitle = title.split(" ")
-    return `${newTitle[0]} ${newTitle[1]}`
-}
+  const newTitle = title.split(" ");
+  return `${newTitle[0]} ${newTitle[1]}`;
+};
 
-export {shorten}
+const isInCart = (state, id) => {
+  const isCart = !!state.selectedItems.find((item) => item.id === id);
+  return isCart;
+};
+
+const quantityCount = (state, id) => {
+  const index = state.selectedItems.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return false;
+  } else {
+    return state.selectedItems[index].quantity;
+  }
+};
+
+export { shorten, isInCart, quantityCount };
